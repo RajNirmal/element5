f12d292f3b666e6647f28d9d74f384e2354576f1
nirmalraj.m <M_Nirmalraj@cat.com>
2023-07-06 23:43:08 +0530

    Refactoring gameService calculateNeighbourCount logic

--- src/main/kotlin/service/GameService.kt
+++ src/main/kotlin/service/GameService.kt
@@ -13,32 +13,15 @@ class GameService {
     }
 
 
-     fun aliveNeighboursCount(rowIndex: Int, colIndex: Int): Int {
+    fun aliveNeighboursCount(rowIndex: Int, colIndex: Int): Int {
         var neighbourCount = 0
         val left: Int = colIndex - 1
         val up: Int = rowIndex - 1
         val right: Int = colIndex + 1
         val down: Int = rowIndex + 1
 
-         if (right < initialSeed.colLength) { //will check the three right neighbors
-             neighbourCount += initialSeed.cells[rowIndex][right]
-             if (up >= 0) {
-                 neighbourCount += initialSeed.cells[up][right]
-             }
-             if (down < initialSeed.rowLength) {
-                 neighbourCount += initialSeed.cells[down][right]
-             }
-         }
-         if (left >= 0) {
-             neighbourCount += initialSeed.cells[rowIndex][left]
-             if (up >= 0) {
-                 neighbourCount += initialSeed.cells[up][left]
-             }
-             if (down < initialSeed.rowLength) {
-                 neighbourCount += initialSeed.cells[down][left]
-             }
-         }
-
+        neighbourCount += leftNeighbourCount(left, rowIndex, up, down)
+        neighbourCount += rightNeighbourCount(right, rowIndex, up, down)
         if (up >= 0) {
             //Neighbour count of cells above
             neighbourCount += initialSeed.cells[up][colIndex]
@@ -48,6 +31,44 @@ class GameService {
             neighbourCount += initialSeed.cells[down][colIndex]
         }
 
+        return neighbourCount
+    }
+
+    private fun rightNeighbourCount(
+        right: Int,
+        rowIndex: Int,
+        up: Int,
+        down: Int
+    ): Int {
+        var neighbourCount = 0
+        if (right < initialSeed.colLength) { //will check the three right neighbors
+            neighbourCount += initialSeed.cells[rowIndex][right]
+            if (up >= 0) {
+                neighbourCount += initialSeed.cells[up][right]
+            }
+            if (down < initialSeed.rowLength) {
+                neighbourCount += initialSeed.cells[down][right]
+            }
+        }
+        return neighbourCount
+    }
+
+    private fun leftNeighbourCount(
+        left: Int,
+        rowIndex: Int,
+        up: Int,
+        down: Int
+    ): Int {
+        var neighbourCount = 0
+        if (left >= 0) {
+            neighbourCount += initialSeed.cells[rowIndex][left]
+            if (up >= 0) {
+                neighbourCount += initialSeed.cells[up][left]
+            }
+            if (down < initialSeed.rowLength) {
+                neighbourCount += initialSeed.cells[down][left]
+            }
+        }
         return neighbourCount
     }
 
